f8e467ead751900c4eae79b2127ab894d12fd96c
KT <thun.kristian@gmail.com>
2023-09-16 10:29:20 +0200

    Refactor for user of map in higehstScore

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -6,8 +8,9 @@ public class Main {
         public static void main (String[]args){
             Scanner sc = new Scanner(System.in);
 
-            ArrayList<Integer> points = new ArrayList<>();
+//            ArrayList<Integer> points = new ArrayList<>();
             ArrayList<String> names = new ArrayList<>();
+            HashMap<String, Integer> userAndPoints = new HashMap<>();
 
             boolean oneMore = true;
 
@@ -15,21 +18,40 @@ public class Main {
                 System.out.println("Skriv namn. Om du vill avlsuta tryck Enter.");
                 String name = sc.nextLine();
                 if (!name.isEmpty()) {
-                    names.add(name);
-                    // Comment2
+//                    names.add(name);
                     System.out.println("Skriv in poängen");
-                    points.add(sc.nextInt());
+//                    points.add(sc.nextInt());
+                    int points = sc.nextInt();
                     sc.nextLine();
+                    if (!userAndPoints.containsKey(name)){
+                        userAndPoints.put(name, points);
+                    }
 //                System.out.println("Vill du lägga till en till?");
 //                String again = sc.nextLine();
                 } else {
                     oneMore = false;
                 }
             }
-            printHighestScore(points, names);
-            printAverageScore(points);
-        }
+            System.out.println(userAndPoints);
+//            printHighestScore(points, names);
+            String userWithHighestPoints = getUserWithHighestPoints(userAndPoints);
+            System.out.println("Användaren med högst poäng var " + userWithHighestPoints + ". Poängen var " + userAndPoints.get(userWithHighestPoints));
+//            printAverageScore(points);
 
+
+        }
+        static String getUserWithHighestPoints(HashMap<String, Integer> userAndPoints){
+            int highestPoints = 0;
+            String nameOfHighestPoints = "";
+            for (Map.Entry<String, Integer> entry : userAndPoints.entrySet()){
+                int points = entry.getValue();
+                if (points > highestPoints){
+                    highestPoints = points;
+                    nameOfHighestPoints = entry.getKey();
+                }
+            }
+            return nameOfHighestPoints;
+        }
 
 
         static void printHighestScore(ArrayList<Integer> points, ArrayList<String> names) {
