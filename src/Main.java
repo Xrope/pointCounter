0244cb487361d127579f0433e13c652f6291ef62
KT <thun.kristian@gmail.com>
2023-09-16 12:14:48 +0200

    Added menu

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,48 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
+    static Scanner sc = new Scanner(System.in);
 
         public static void main (String[]args){
-            Scanner sc = new Scanner(System.in);
+            HashMap<String, Integer> nameAndPoints = new HashMap<>();
+            boolean keepGoing = true;
+            System.out.println("Välkommen!");
+            System.out.println("Här kan du skriva in namn och poäng och sedan räkna medelvärdet och ta reda på högst poängen.");
+
+            do {
+                String menuItemPicked = "";
+                System.out.println();
+                printMenu(nameAndPoints.isEmpty());
+                menuItemPicked = sc.nextLine();
+
+                switch (menuItemPicked) {
+                    case "1" -> addNamesAndPoints(nameAndPoints);
+                    case "2" -> userWithHighestPoints(nameAndPoints);
+                    case "3" -> printAverageScoreMap(nameAndPoints);
+                    case "4" -> keepGoing = false;
+                    default -> System.out.println("Ogiltigt menyval");
+                }
 
 
-            ArrayList<String> names = new ArrayList<>();
-            HashMap<String, Integer> userAndPoints = new HashMap<>();
+            }while (keepGoing);
 
+
+            System.out.println(nameAndPoints);
+
+        }
+
+        static void printMenu(boolean isNameAndPointsEmpty){
+            System.out.println("___________Meny___________");
+            System.out.println("1. Lägg in namn och poäng");
+            System.out.println("2. Visa vem som har högstpoäng." + (isNameAndPointsEmpty ? "Lägg in namn och poäng först.": ""));
+            System.out.println("3. Räkna ut medelvärdet." + (isNameAndPointsEmpty ? "Lägg in namn och poäng först.": ""));
+            System.out.println(("4. Avsluta"));
+
+        }
+
+        static void addNamesAndPoints(HashMap<String, Integer> nameAndPoints){
             boolean oneMore = true;
+
 
             while (oneMore) {
                 System.out.println("Skriv namn. Om du vill avlsuta tryck Enter.");
@@ -23,8 +56,8 @@ public class Main {
 
                     int points = sc.nextInt();
                     sc.nextLine();
-                    if (!userAndPoints.containsKey(name)){
-                        userAndPoints.put(name, points);
+                    if (!nameAndPoints.containsKey(name)){
+                        nameAndPoints.put(name, points);
                     }
 //                System.out.println("Vill du lägga till en till?");
 //                String again = sc.nextLine();
@@ -32,14 +65,8 @@ public class Main {
                     oneMore = false;
                 }
             }
-            System.out.println(userAndPoints);
-
-            userWithHighestPoints(userAndPoints);
-            printAverageScoreMap(userAndPoints);
-
-
-
         }
+
         static void userWithHighestPoints(HashMap<String, Integer> userAndPoints){
             int highestPoints = 0;
             String nameOfHighestPoints = "";
@@ -52,7 +79,7 @@ public class Main {
             }
             System.out.println("Personen med högst poäng var " + nameOfHighestPoints + ". Poängen var " + highestPoints);
         }
-        
+
     static void printAverageScoreMap(HashMap<String, Integer> nameAndPoints) {
         double sumOfPoints = 0;
 
